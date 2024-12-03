8f242e291358e3f350fc756b6a384e320c73fbf0
LENOVO <LENOVO@LAPTOP-OCJJ5498>
2024-12-03 09:27:59 +0800

    second commit

---
+++ src/reservasihotel2/ConnectionProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reservasihotel2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+
+        return null;
+
+    }
+}
